203a2ad9ef0c0e90097b115abd804209c9b01f6f
Kohle <me@kohle.co>
2014-10-22 14:16:40 -0400

    Barebones

---
+++ src/co/kohle/ricomuerte/RicoMuerte.java
@@ -0,0 +1,35 @@
+/*
+ * This file is part of RicoMuerte.
+ *
+ * Copyright (c) 2014 Kohle <http://kohle.co>
+ * RicoMuerte is licensed under the Mozilla Public License 2.0.
+ *
+ * RicoMuerte is free software: you can redistribute it and/or modify
+ * it under the terms of the Mozilla Public License as published by
+ * the Mozilla project.
+ *
+ * RicoMuerte is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY. See the license for more details.
+ *
+ * You should have received a copy of the Mozilla Public License 2.0
+ * along with this program.  If not, see <https://www.mozilla.org/MPL/2.0/>.
+ */
+
+package co.kohle.ricomuerte;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Level;
+
+public class RicoMuerte extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().log(Level.INFO, "Enabled.");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().log(Level.INFO, "Disabled.");
+    }
+}
